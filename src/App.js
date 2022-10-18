63db67cdccc565e57979c99f31fea42d6c149bf6
rajutsav1234 <39861403+rajutsav1234@users.noreply.github.com>
2022-10-18 08:25:17 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [long_link, set_long_link]=useState(null);
+  const [display_link, set_display_link]=useState(null);
+  function get_long_link(val){
+    set_long_link(val.target.value);
+  }
+  const func1 = () => {
+    fetch("https://api-ssl.bitly.com/v4/shorten", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer 2d1739419f49115afad641f11c4cf90cc5ecf36c`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        long_url: long_link,
+        domain: "bit.ly",
+      }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      set_display_link(data.link);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header glow">
+        Link Shortner
+      </div>
+      <div class='bottom-part'>
+        <input type="text" placeholder="Enter the URL" onChange={get_long_link} onKeyDown={func1} ></input>
+        <button onClick={func1}>
+          Shorten
+        </button>
+        <br />
+        <div className="border">
+          <span id="id1">{display_link}</span>
+          <button id="id2" onClick={() =>  navigator.clipboard.writeText(document.getElementById("id1").innerText)}>Copy</button>
+        </div>
+      </div>
     </div>
   );
 }
